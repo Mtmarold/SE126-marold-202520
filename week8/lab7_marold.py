1fb7adb99afef01c07b59f03244c50bf89dcef80
Mtmarold <mtmarold@email.neit.edu>
2025-03-03 10:23:13 -0500

    Update lab7_marold.py

--- week8/lab7_marold.py
+++ week8/lab7_marold.py
@@ -10,9 +10,8 @@ import csv
 
 #--- Variable Dictionary ---
 # Function to load words from the CSV file into a dictionary
-def load_words_from_csv(file_path):
+def load_dict(file_path):
     word_dict = {}
-    file_path = 'text_files/words.csv'
     with open(file_path, mode='r', newline='', encoding='utf-8') as file:
         reader = csv.reader(file)
         next(reader)  # Skip header row if there's one
@@ -22,6 +21,16 @@ def load_words_from_csv(file_path):
             word_dict[word] = definition
     return word_dict
 
+def show_all_words(word_dict):
+    if word_dict:
+        print("---PROGRAMMING DICTIONARY---")
+        print("-" * 80)
+        for word, definition in dictionary.items():
+            print()
+            print(f"{word.upper()}: {definition}")
+        print("-" * 80)
+    else:
+        print("Dictionary is Empty!")
 
 # Function to display the menu and interact with the dictionary
 def display_menu():
@@ -35,12 +44,43 @@ def display_menu():
 def search_word(word_dict):
     word = input("Enter the word to search for: ").strip().lower()
     if word in word_dict:
+        print("-" * 80)
         print(f"{word.capitalize()}: {word_dict[word]}")
+        print("-" * 80)
     else:
         print("Word not found!")
 
 def add_word(word_dict):
-    word = input("Enter The word You would like to add: ")
-    if word not in word_dict:
+    word = input("Enter The word You would like to add: ").strip().lower()
+    if word in word_dict:
         print()
+        print(f"'{word}' already exists in the dictionary.")
+    else:
+        definition = input(f"Enter the definition for {word}: ").strip()
+        word_dict[word] = definition
+        print("-" * 80)
+        print(f"The word and definition have been added to the dictionary: {word.capitalize()}: {word_dict[word]}")
+        print("-" * 80)
 #--- MAIN CODE BELOW ---
+
+file_path = "text_files/words.csv"
+dictionary = load_dict(file_path)
+answer = "y"
+
+while answer == "y":
+    display_menu()
+
+    choice = input("Select an option [1-4]: ").strip()
+
+    if choice == "1":
+        show_all_words(dictionary)
+    elif choice == "2":
+        search_word(dictionary)
+    elif choice == "3":
+        add_word(dictionary)
+    elif choice == "4":
+        answer = "n"
+        print("Thank you for using my program, goodbye :)")
+    
+    else:
+        print("Invalid choice. Please select a valid option [1-4]")
